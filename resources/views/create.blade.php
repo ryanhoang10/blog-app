28d65b34e902609d9f8b74bae030ad1f75c8da01
Ryan Hoang <55223126+ryanhoang10@users.noreply.github.com>
2022-03-31 14:15:43 -0500

    Use blade template correctly

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,17 +1,8 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="{{ mix('/js/app.js') }}"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+@extends('layout')
 
-</head>
-<body>
+@section('content')
     <h3 class="leading-tight text-5xl mt-0 mb-2 text-emerald-700 mb-2 text-center">Create a Blog</h3>
     <div id="app">
         <create-blog :categories='@json($blogCategories)'> </create-blog>
     </div>
-</body>
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Blog Project</title>
-
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ mix('/js/app.js') }}"></script>
-
-    </head>
-    <body>
-        <div id="app">
-            @php
-                $text = 'Welcome to the Blog page';   
-            @endphp
-            <site-header :categories='@json($blogCategories)' :title='@json($text)'></site-header>
-            <home :blogs='@json($blogs)'></home>
-         </div>
-    </body>
-</html>
+@section('content')
+    @php
+        $text = 'Welcome to the Blog page';   
+    @endphp
+    <site-header :categories='@json($blogCategories)' :title='@json($text)'></site-header>
+    <home :blogs='@json($blogs)'></home>
+@endsection
