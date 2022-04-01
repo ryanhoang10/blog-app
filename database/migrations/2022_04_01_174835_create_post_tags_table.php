e90f621495e48b8ec155b93b87880d57ed44d310
Ryan Hoang <55223126+ryanhoang10@users.noreply.github.com>
2022-04-01 12:54:29 -0500

    Create 2022_04_01_174835_create_post_tags_table.php

---
+++ database/migrations/2022_04_01_174835_create_post_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tags', function (Blueprint $table) {
+            $table->primary(['blog_id', 'tag_id']);
+            $table->unsignedInteger('blog_id');
+            $table->unsignedInteger('tag_id');
+
+            $table->foreign('blog_id')->references('id')->on('blogs')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tags');
+    }
+};
