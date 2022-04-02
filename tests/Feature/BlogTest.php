258a2d2f10d4d6bcaaa0b6b16318b069573c6dc9
Ryan Hoang <55223126+ryanhoang10@users.noreply.github.com>
2022-04-01 20:40:54 -0500

    Setting up basic assertions

--- tests/Feature/BlogTest.php
+++ tests/Feature/BlogTest.php
@@ -2,16 +2,34 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Blog;
 use Tests\TestCase;
 
 class BlogTest extends TestCase
 {
     /** @test */
-    public function the_blog_home_page_has_correct_header()
+    public function the_index_returns_a_200_status()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_blog_contains_the_correct_data()
+    {
+        $blog = Blog::factory()->create([
+            'title'     => 'test_title',
+            'body'      => 'test_body',
+            'user'      => 'test_user',
+            'category'  => 'test_category',
+            'author'    => 'test_author',
+        ]);
+
+        $this->assertEquals('test_title', $blog->title);
+        $this->assertEquals('test_body', $blog->body);
+        $this->assertEquals('test_user', $blog->user);
+        $this->assertEquals('test_category', $blog->category);
+        $this->assertEquals('test_author', $blog->author);
     }
 }
